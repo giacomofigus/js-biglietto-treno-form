8300e80a295dfc58ed1a8803c0bac25229b3235e
MyUsername <figusgiacomo04@gmail.com>
2024-01-09 12:53:00 +0100

    fix:

--- main.js
+++ main.js
@@ -1,13 +1,53 @@
-/*
-    MILESTONE 1:
-    Iniziamo implementando il programma senza alcuna estetica: usando esclusivamente due input e un bottone (non stilizzati), realizziamo le specifiche scritte sopra. La risposta finale (o output) sarà anch’essa da scrivere in console.
 
-    MILESTONE 2:
-    Solo una volta che il milestone 1 sarà completo e funzionante allora realizzeremo un form in pagina in cui l’utente potrà inserire i dati e visualizzare il calcolo finale con il prezzo.
-    Il recap dei dati e l'output del prezzo finale, andranno quindi stampati in pagina (il prezzo dovrà essere formattato con massimo due decimali, per indicare i centesimi sul prezzo). Questo richiederà un minimo di ricerca.
- */
+const nomeCognomeHtml = document.getElementById('nameSurname')
+const kmHtml = document.getElementById('km')
+const etaHtml = document.getElementById('eta')
+const generaRisultato = document.getElementById('btnRisultato')
+const carrozzaHtml = document.getElementById('numeroCarrozza')
+const codiceCpHtml = document.getElementById('numeroCodiceCp')
 
-function validateForm(){
-    let nomeUtente = document.getElementById("fName");
-    console.log(nomeUtente.value);
+const h2Html = document.getElementById('titoloBiglietto')
+const bigliettoHtml = document.getElementById('biglietto')
+const risultatoNomeCognomeHtml = document.getElementById('risultatoNomeCognome')
+const risultatoOffertaHtmll = document.getElementById('risultatoOfferta')
+
+h2Html.style.display = "none"
+bigliettoHtml.style.display = "none"
+
+
+generaRisultato.addEventListener('click', function(){
+  // document.querySelector('h2.d-none').classList.remove('d-none')
+  // document.querySelector('div.d-none').classList.remove('d-none')
+
+  h2Html.style.display = "block"
+  bigliettoHtml.style.display = "block"
+
+  let codiceCarrozza = RandomNumber (0,10)
+  let codiceCp = RandomNumber (100,10000)
+
+  carrozzaHtml.innerHtml = codiceCarrozza
+  codiceCpHtml.innerHTML = codiceCp
+
+ 
+ risultatonNomeCognomeHtml.innerHTML = nomeCognomeHtml.value
+
+ if(etaHtml.value === "minorenne"){
+    risultatoOffertaHtml.innerHTML = "biglietto scont 20%"
+
+
+ } else if(etaHtml.value === "maggiorenne"){
+  risultatoOffertaHtml.innerHTML = "biglietto "
+
+
+ } else if(etaHtml.value === "over"){
+  risultatoOffertaHtml.innerHTML = "biglietto scont 40%"
+
+
+ }
+
+})
+
+function RandomNumber(min, max){
+
+  return Math.floor(Math.random()*(max-min + 1) + min)
 }
